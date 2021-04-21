2bdbce9a23f99f9d50fef517a8ed1482e2240796
Graff Médéric <graffmederic@gmail.com>
2021-04-21 17:58:38 +0200

    Modifications JS panier.html: affichage dynamique du montant total du panier, ajout d'un icône poubelle en lieu et place du bouton de suppression d'un article

--- js/panier.js
+++ js/panier.js
@@ -25,16 +25,47 @@ if (produitLocalStorage === null || produitLocalStorage == 0) {
         structurePanierProduit = structurePanierProduit + 
         
         `
-        <div class="container-recapitulatif">
-            <div><span class="gras">Quantité :</span> ${produitLocalStorage[i].quantite} <span class="gras"> - Produit : </span> ${produitLocalStorage[i].name} - ${produitLocalStorage[i].objectif}</div>
-            <div>
-                <div class="prix-produit"><span class="gras">${produitLocalStorage[i].price}€</span></div> 
-                <div><button class="btn-supprimer"> supprimer </button></div>
-            </div>
-      </div>   
+        <tr class="container-recapitulatif">
+            <td class="recap-produit"> ${produitLocalStorage[i].name}</td>
+            <td class="recap-objectif"> ${produitLocalStorage[i].objectif}</td>
+            <td class="recap-quantite"> ${produitLocalStorage[i].quantite}</td>
+            <td class="recap-prix">${produitLocalStorage[i].price/100}€</td> 
+            <td><button class="btn-supprimer"><i class="fas fa-trash-alt"></i></button></td>
+        </tr>   
         ` ;
 
     }  
         // Injection du code html dans le panier
         panierProduit.innerHTML = structurePanierProduit;
 }
+
+// --- TRY --- : Affichage du montant total du panier
+
+// Déclaration de la variable du prix total
+let prixTotalPanier = [];
+
+// Récupération des données de prix dans le localStorage
+for (let j = 0; j < produitLocalStorage.length; j++) {
+    let prixProduitsPanier = produitLocalStorage[j].price/100;
+    // Ajout des données de prix dans la variable du prix total du panier
+    prixTotalPanier.push(prixProduitsPanier);
+    console.log(prixTotalPanier);
+}
+
+// Calcul du prix global se trouvant dans la variable prixTotalPanier à l'aide de la méthode "reduce()"
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+const sommePanier = prixTotalPanier.reduce(reducer, 0);
+console.log(sommePanier);
+
+// Affichage du résultat dans la page html
+document.getElementById("panier-prix").innerHTML = sommePanier;
+
+
+
+
+
+
+/* try
+let test = sommePanier;
+document.getElementById("panier-prix").value = test;
+*/
